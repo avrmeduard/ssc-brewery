345aa115cf00f289d00bb5ed047f9818ecd3a924
avrm <avrmeduard@gmail.com>
2021-02-18 23:09:22 +0200

    + find beer by upc

--- src/test/java/guru/sfg/brewery/web/controllers/BerrRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BerrRestControllerIT.java
@@ -18,13 +18,13 @@ public class BerrRestControllerIT extends BaseIT{
 
     @Test
     void findBerrById() throws Exception {
-        mockMvc.perform(get("/api/v1/beer/spring"))
+        mockMvc.perform(get("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311"))
                 .andExpect(status().isOk());
     }
 
     @Test
     void findBeerByUpc() throws Exception {
-        mockMvc.perform(get("/api/v1/beer/0631234200036"))
+        mockMvc.perform(get("/api/v1/beerUpc/0631234200036"))
                 .andExpect(status().isOk());
     }
 }
