c5846f19a8c3df23dbb9a1a4c655673cbbc3c9b9
avrm <avrmeduard@gmail.com>
2021-02-19 14:32:10 +0200

    +

--- src/test/java/guru/sfg/brewery/web/controllers/BerrRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BerrRestControllerIT.java
@@ -3,6 +3,7 @@ package guru.sfg.brewery.web.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
